7e53dd50f7a7bb7f1fc57d983230b9cbd23cf720
Ahmet aytangil <ahmet.aytangil@dop.com.tr>
2023-07-04 15:03:08 +0300

    desktop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -45,8 +45,6 @@ const maskConfig: Partial<IConfig> = {
     NgxMaskModule.forRoot(maskConfig),
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
     }),
 
